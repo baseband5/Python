e473fe8f25a643936719ab413fb00807afc1da7c
baseband5 <rejestracja@krzes.com>
2021-12-13 09:55:01 +0100

    delete duplicates

--- Lec1/__init__.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-from .e1 import *
-
-def main():
-  print(__name__ +" : exercise 1:")
-  e1.main()
--- Lec2/__init__.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-from .e2 import *
-
-def main():
-  print(__name__ +" : exercise 2:")
-  e2.main()
